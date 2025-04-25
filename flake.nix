938307e08d51ab589a96d392ef6a8b0650aa7f24
Euan Kemp <euank@euank.com>
2025-04-25 22:39:23 +0900

    update flake.nix to support x86_64-linux too
    More arches is better maybe or something

--- flake.nix
+++ flake.nix
@@ -3,57 +3,70 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        yarn-berry-3-fetcher = pkgs.callPackage (
+          {
+            rustPlatform,
+            pkg-config,
+            libzip,
+            openssl,
+            YARN_ZIP_SUPPORTED_CACHE_VERSION ? 8,
+          }:
 
-    packages.aarch64-linux.yarn-berry-3-fetcher = nixpkgs.legacyPackages.aarch64-linux.callPackage (
-      {
-        rustPlatform,
-        pkg-config,
-        libzip,
-        openssl,
-        YARN_ZIP_SUPPORTED_CACHE_VERSION ? 8,
-      }:
+          rustPlatform.buildRustPackage {
+            pname = "yarn-berry-fetcher";
+            version = "1.2.1";
 
-      rustPlatform.buildRustPackage {
-        pname = "yarn-berry-fetcher";
-        version = "1.2.1";
+            src = self;
 
-        src = self;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              allowBuiltinFetchGit = true;
+            };
 
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-          allowBuiltinFetchGit = true;
-        };
+            LIBZIP_SYS_USE_PKG_CONFIG = 1;
+            inherit YARN_ZIP_SUPPORTED_CACHE_VERSION;
 
-        LIBZIP_SYS_USE_PKG_CONFIG = 1;
-        inherit YARN_ZIP_SUPPORTED_CACHE_VERSION;
-
-        nativeBuildInputs = [
-          rustPlatform.bindgenHook
-          pkg-config
-        ];
-
-        buildInputs = [
-          (libzip.overrideAttrs rec {
-            patches = libzip.patches ++ [
-              ./libzip-revert-to-old-versionneeded-behavior.patch
+            nativeBuildInputs = [
+              rustPlatform.bindgenHook
+              pkg-config
             ];
-          })
-          openssl
-        ];
+
+            buildInputs = [
+              (libzip.overrideAttrs {
+                patches = libzip.patches ++ [
+                  ./libzip-revert-to-old-versionneeded-behavior.patch
+                ];
+              })
+              openssl
+            ];
+          }
+        ) { };
+        yarn-berry-4-fetcher = yarn-berry-3-fetcher.override {
+          libzip = pkgs.libzip.override {
+            zlib = pkgs.zlib-ng.override { withZlibCompat = true; };
+          };
+          YARN_ZIP_SUPPORTED_CACHE_VERSION = 10;
+        };
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+        packages = {
+          inherit yarn-berry-4-fetcher yarn-berry-3-fetcher;
+          default = yarn-berry-4-fetcher;
+        };
       }
-    ) {};
-
-    packages.aarch64-linux.yarn-berry-4-fetcher = self.packages.aarch64-linux.yarn-berry-3-fetcher.override {
-      libzip = nixpkgs.legacyPackages.aarch64-linux.libzip.override {
-        zlib = nixpkgs.legacyPackages.aarch64-linux.zlib-ng.override { withZlibCompat = true; };
-      };
-      YARN_ZIP_SUPPORTED_CACHE_VERSION = 10;
-    };
-
-    packages.aarch64-linux.default = self.packages.aarch64-linux.yarn-berry-4-fetcher;
-
-  };
+    );
 }
